1da471e49e398d3fc2b0b7f36c51245cf8a06364
Gary Mansell <gary.mansell@cyberlytic.com>
2017-06-26 15:21:11 +0100

    Added basic webserver implementation.

--- buildAndRun.ps1
+++ buildAndRun.ps1
@@ -1,9 +1,22 @@
-$outputExe = "$(Get-Location)\target\debug\rust-challenge.exe"
+param (
+    [switch]$clean = $false,
+    [switch]$execute = $false,
+    [string]$outputExe = "$(Get-Location)\target\debug\rust-challenge.exe"
+)
 
-cargo clean ; cargo build
-if (Test-Path $outputExe) {
+if ($clean) {
+    Write-Host "Cleaning..." -ForegroundColor Green
+    cargo clean
+} 
+
+Write-Host "Building..." -ForegroundColor Green
+cargo build
+
+if ($execute) {
     Write-Host "Executing..." -ForegroundColor Green
-    .\target\debug\rust-challenge.exe
-} else {
-    Write-Host "Failed!" -ForegroundColor Red
+    if (Test-Path $outputExe) {        
+        .\target\debug\rust-challenge.exe
+    } else {
+        Write-Host "Failed!" -ForegroundColor Red
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate hyper;
+extern crate futures;
+
+use hyper::header::{ContentLength, From};
+use hyper::server::{Http, Request, Response, Service};
+use hyper::{Method, StatusCode};
+
+static NOT_FOUND: &'static str = "NOT FOUND";
+
+struct RustService;
+
+impl Service for RustService {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        let mut response = Response::new();
+        match (request.method(), request.path()) {
+            (&Method::Get, "/") => {
+                let text: &'static str = "This is a basic Rust web service.";
+                response.headers_mut().set(From("Moi!!".parse().unwrap()));
+                response.headers_mut().set(ContentLength(text.len() as u64));
+                response.set_body(text);
+            },
+            (&Method::Post, "/command") => {
+                response.set_body(request.body());
+            },
+            _ => {
+                response.set_body(NOT_FOUND);
+                response.set_status(StatusCode::NotFound);
+            }
+        };
+        futures::future::ok(response)
+    }
+}
+
 fn main() {
-    print!("Hello world!");
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(RustService)).unwrap();
+    server.run().unwrap();
 }
